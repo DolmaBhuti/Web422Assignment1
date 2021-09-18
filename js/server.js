2332d3ef3adad4a411691da1382a607352a94233
Dolma Bhuti <dbhuti1@myseneca.ca>
2021-09-17 22:06:40 -0400

    added git ignore file

--- js/server.js
+++ js/server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+* WEB422 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Dolma Bhuti Student ID: 0154381398 Date: 17/09/2021
+* Heroku Link:  https://git.heroku.com/boiling-chamber-82558.git
+*
+********************************************************************************/
+
 const express = require('express')
 const app = express()
 const cors = required("cors")
